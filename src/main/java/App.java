81a263f328465509409da8f5cd154c1155a1dc8f
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-01 19:36:23 -0300

    adiciona aplicação executável

---
+++ src/main/java/App.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InterpretadorDeEntrada interpretadorDeEntrada = new InterpretadorDeEntrada();
+        ValidadorDeMoedas validadorDeMoedas = new ValidadorDeMoedas();
+        CalculadoraDeCambio calculadoraDeCambio = new CalculadoraDeCambio();
+        FormatadorDeSaida formatadorDeSaida = new FormatadorDeSaida();
+
+        System.out.println("Digite a moeda de origem, o valor e moeda de destino: ");
+        String entrada = scanner.nextLine();
+
+        String moedaInicial = interpretadorDeEntrada.extrairMoedaInicial(entrada);
+        String moedaFinal = interpretadorDeEntrada.extrairMoedaFinal(entrada);
+        String moedasParaConversao = interpretadorDeEntrada.detectarMoedaParaConversao(moedaInicial, moedaFinal);
+
+        Boolean validacaoMoedaInicial = validadorDeMoedas.validarMoeda(moedaInicial);
+        Boolean validacaoMoedaFinal = validadorDeMoedas.validarMoeda(moedaFinal);
+
+        if (!validacaoMoedaInicial){
+            System.out.println("Moeda não suportada: " + moedaInicial);
+        }
+        if (!validacaoMoedaFinal){
+            System.out.println("Moeda não suportada: " + moedaFinal);
+        }
+
+        Double valor = interpretadorDeEntrada.extrairValor(entrada);
+        Double taxaDeCambio = calculadoraDeCambio.determinarTaxaDeCambio(moedasParaConversao);
+        Double resultado = calculadoraDeCambio.calcularValorFinal(valor, taxaDeCambio);
+
+        System.out.println(formatadorDeSaida.formatarSaida(moedaFinal, resultado));
+
+        scanner.close();
+    }
+
+}
