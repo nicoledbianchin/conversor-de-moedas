df83c2117dd060fb3c9a42554af7d86363266d5c
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-01 18:56:36 -0300

    implementa um teste completo

--- src/test/java/TestesFinais.java
+++ src/test/java/TestesFinais.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestesFinais {
+
+    private InterpretadorDeEntrada interpretadorDeEntrada = new InterpretadorDeEntrada();
+    private ValidadorDeMoedas validadorDeMoedas = new ValidadorDeMoedas();
+    private CalculadoraDeCambio calculadoraDeCambio = new CalculadoraDeCambio();
+
+    @Test
+    public void calculoFinalApenasUmRetorno(){
+        String entrada = "BRL100 CLP";
+        String moedaInicial = interpretadorDeEntrada.extrairMoedaInicial(entrada);
+        String moedaFinal = interpretadorDeEntrada.extrairMoedaFinal(entrada);
+        String moedasConversao = interpretadorDeEntrada.detectarMoedaParaConversao(moedaInicial, moedaFinal);
+        Double valor = interpretadorDeEntrada.extrairValor(entrada);
+
+        Boolean validacaoInicial = validadorDeMoedas.validarMoeda(moedaInicial);
+        Boolean validacaoFinal = validadorDeMoedas.validarMoeda(moedaFinal);
+        Double taxa = calculadoraDeCambio.determinarTaxaDeCambio(moedasConversao);
+        Double resultado = calculadoraDeCambio.calcularValorFinal(valor, taxa);
+
+        Assert.assertEquals(true, validacaoInicial);
+        Assert.assertEquals(true, validacaoFinal);
+        Assert.assertEquals(java.util.Optional.of(175.65), java.util.Optional.of(taxa));
+        Assert.assertEquals(java.util.Optional.of(17565.0), java.util.Optional.of(resultado));
+    }
+
+    @Test
+    public void calculoDoisRetornos(){
+        String entrada = "BRL100 CLP EUR";
+        String moedaInicial = interpretadorDeEntrada.extrairMoedaInicial(entrada);
+        String moedaFinal = interpretadorDeEntrada.extrairMoedaFinal(entrada);
+        String moedasConversao = interpretadorDeEntrada.detectarMoedaParaConversao(moedaInicial, moedaFinal);
+        Double valor = interpretadorDeEntrada.extrairValor(entrada);
+
+
+    }
+
 }
