40e2d3f641eea3db724c57577625ed7ae4d0268b
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-30 23:47:15 -0300

    implementa método de extrair valor da entrada

--- src/main/java/InterpretadorDeEntrada.java
+++ src/main/java/InterpretadorDeEntrada.java
@@ -1,9 +1,15 @@
 public class InterpretadorDeEntrada {
 
     String moedaInicial;
+    Double valor;
 
     public String extrairMoedaInicial(String entrada) {
         moedaInicial = entrada.substring(0, 3);
         return moedaInicial;
+    }
+
+    public Double extrairValor(String entrada){
+        valor = Double.parseDouble(entrada.replaceAll("[^0-9]*", ""));
+        return valor;
     }
 }
