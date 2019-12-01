ad0fa049ab1e89e7fb1b70179b659c833f722e84
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-01 15:53:35 -0300

    adiciona metodo de calcular resultado final

--- src/main/java/CalculadoraDeCambio.java
+++ src/main/java/CalculadoraDeCambio.java
@@ -3,12 +3,31 @@ public class CalculadoraDeCambio {
     private Double taxaDeCambio;
 
     public Double determinarTaxaDeCambio(String moedasParaConversao){
+        if (moedasParaConversao.length() > 6){
+            determinarTaxasDeCambio(moedasParaConversao);
+        }
+
+        return null;
+    }
+
+    public void determinarTaxasDeCambio(String moedasParaConversao){
+        String[] lista = moedasParaConversao.split(" ");
+        for (int i = 0; i < lista.length; i++) {
+            percorrerListaDeTaxas(lista[i]);
+        }
+    }
+
+    public Double percorrerListaDeTaxas(String moedasParaConversao){
         for (TaxaDeCambio taxa : TaxaDeCambio.values()){
             if (moedasParaConversao.equals(taxa.name())){
                 taxaDeCambio = taxa.getValor();
             }
         }
         return taxaDeCambio;
+    }
+
+    public Double calcularValorFinal(Double valor, Double taxaDeCambio){
+        return valor * taxaDeCambio;
     }
 
 }
