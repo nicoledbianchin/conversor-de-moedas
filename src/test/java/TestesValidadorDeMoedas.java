1f7a6d692fe1f76031417fea3d0572c5d9af25dd
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-12-07 12:53:44 -0300

    adiciona testes de validar mais de uma moeda

--- src/test/java/TestesValidadorDeMoedas.java
+++ src/test/java/TestesValidadorDeMoedas.java
@@ -10,7 +10,7 @@ public class TestesValidadorDeMoedas {
     public void testaMoedaValida(){
         String moeda = "EUR";
 
-        Boolean valida = validador.validarUmaMoeda(moeda);
+        Boolean valida = validador.validarMoeda(moeda);
 
         Assert.assertEquals(true, valida);
     }
@@ -19,7 +19,61 @@ public class TestesValidadorDeMoedas {
     public void testaMoedaInvalida(){
         String moeda = "ARS";
 
-        Boolean invalida = validador.validarUmaMoeda(moeda);
+        Boolean invalida = validador.validarMoeda(moeda);
+
+        Assert.assertEquals(false, invalida);
+    }
+
+    @Test
+    public void testaValidarDuasMoedasValidas(){
+        String moeda = "BRL EUR";
+
+        Boolean valida = validador.validarMoeda(moeda);
+
+        Assert.assertEquals(true, valida);
+    }
+
+    @Test
+    public void testaValidarTresMoedasValidas(){
+        String moeda = "EUR USD CLP";
+
+        Boolean valida = validador.validarMoeda(moeda);
+
+        Assert.assertEquals(true, valida);
+    }
+
+    @Test
+    public void testaValidarUmaMoedaValidaEUmaInvalida(){
+        String moeda = "EUR ASD";
+
+        Boolean invalida = validador.validarMoeda(moeda);
+
+        Assert.assertEquals(false, invalida);
+    }
+
+    @Test
+    public void testaValidarDuasMoedasValidasEUmaInvalida(){
+        String moeda = "EUR ASD BRL";
+
+        Boolean invalida = validador.validarMoeda(moeda);
+
+        Assert.assertEquals(false, invalida);
+    }
+
+    @Test
+    public void testaValidarDuasMoedasInvalidasEUmaValida(){
+        String moeda = "AER ASD BRL";
+
+        Boolean invalida = validador.validarMoeda(moeda);
+
+        Assert.assertEquals(false, invalida);
+    }
+
+    @Test
+    public void testaValidarTresMoedasInvalidas(){
+        String moeda = "AER ASD FDA";
+
+        Boolean invalida = validador.validarMoeda(moeda);
 
         Assert.assertEquals(false, invalida);
     }
