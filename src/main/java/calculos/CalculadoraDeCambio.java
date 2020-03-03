0a67dcb44b3f662352c0219dc61c7c0a7d35afff
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-03 11:04:14 -0300

    retira metodo inutil na calculadora

--- src/main/java/calculos/CalculadoraDeCambio.java
+++ src/main/java/calculos/CalculadoraDeCambio.java
@@ -1,19 +1,6 @@
 package calculos;
 
-import valoresMoedasETaxas.TaxaDeCambio;
-
 public class CalculadoraDeCambio {
-
-    private Double taxaDeCambio;
-
-    public Double determinarTaxaDeCambio(String moedasParaConversao){
-        for (TaxaDeCambio taxa : TaxaDeCambio.values()){
-            if (moedasParaConversao.equals(taxa.name())){
-                taxaDeCambio = taxa.getValor();
-            }
-        }
-        return taxaDeCambio;
-    }
 
     public Double calcularValorFinal(Double valor, Double taxaDeCambio){
         return valor * taxaDeCambio;
