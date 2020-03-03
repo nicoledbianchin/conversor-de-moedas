4a49d07e2cdecdcc058e38e5a8e375893ecff920
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-03 14:00:56 -0300

    refatora teste calculadora

--- src/test/java/TestesCalculadoraDeCambio.java
+++ src/test/java/TestesCalculadoraDeCambio.java
@@ -1,33 +1,17 @@
 import calculos.CalculadoraDeCambio;
 import org.junit.Assert;
 import org.junit.Test;
+import tratamentoDeDados.InterpretadorDeEntrada;
 
 public class TestesCalculadoraDeCambio {
 
     private CalculadoraDeCambio calculadoraDeCambio = new CalculadoraDeCambio();
 
-    @Test
-    public void retornaValorDaTaxa(){
-        String entrada = "BRLEUR";
-
-        Double resultado = calculadoraDeCambio.determinarTaxaDeCambio(entrada);
-
-        Assert.assertEquals(java.util.Optional.of(0.23), java.util.Optional.of(resultado));
-    }
+    InterpretadorDeEntrada interpretadorDeEntrada = new InterpretadorDeEntrada();
 
     @Test
     public void calcularValorFinal(){
         Double resultado = calculadoraDeCambio.calcularValorFinal(100.0, 175.65);
-
-        Assert.assertEquals(java.util.Optional.of(17565.0), java.util.Optional.of(resultado));
-    }
-
-    @Test
-    public void calcularTaxEValorFinal(){
-        String entrada = "BRLCLP";
-
-        Double taxa = calculadoraDeCambio.determinarTaxaDeCambio(entrada);
-        Double resultado = calculadoraDeCambio.calcularValorFinal(100.0, taxa);
 
         Assert.assertEquals(java.util.Optional.of(17565.0), java.util.Optional.of(resultado));
     }
