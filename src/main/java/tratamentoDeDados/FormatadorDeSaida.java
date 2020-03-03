8ac7bb8ba5a8b792d992efcb04fb246f3a0759b8
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-03 14:21:38 -0300

    refatora formatador de saida

--- src/main/java/tratamentoDeDados/FormatadorDeSaida.java
+++ src/main/java/tratamentoDeDados/FormatadorDeSaida.java
@@ -1,9 +1,11 @@
 package tratamentoDeDados;
 
+import valoresMoedasETaxas.Moeda;
+
 public class FormatadorDeSaida {
 
-    public String formatarSaida(String moedaFinal, Double resultado){
-        return moedaFinal + resultado;
+    public String formatarSaida(Moeda moedaFinal, Double resultado){
+        return moedaFinal.getMoeda() + resultado + " ";
     }
 
 }
