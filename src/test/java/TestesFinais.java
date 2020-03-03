dc38b85bca0820b8b1f1e3e81b6623cf7a1ff083
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-03 14:11:18 -0300

    faz teste com interpretador de saida

--- src/test/java/TestesFinais.java
+++ src/test/java/TestesFinais.java
@@ -1,6 +1,7 @@
 import calculos.CalculadoraDeCambio;
 import org.junit.Assert;
 import org.junit.Test;
+import tratamentoDeDados.FormatadorDeSaida;
 import tratamentoDeDados.InterpretadorDeEntrada;
 import valoresMoedasETaxas.Moeda;
 
@@ -10,6 +11,7 @@ public class TestesFinais {
 
     private InterpretadorDeEntrada interpretadorDeEntrada = new InterpretadorDeEntrada();
     private CalculadoraDeCambio calculadoraDeCambio = new CalculadoraDeCambio();
+    private FormatadorDeSaida formatadorDeSaida = new FormatadorDeSaida();
 
     @Test
     public void calculoFinalApenasUmRetorno(){
@@ -29,6 +31,27 @@ public class TestesFinais {
 
         Assert.assertEquals(java.util.Optional.of(175.65), java.util.Optional.of(taxa));
         Assert.assertEquals(java.util.Optional.of(17565.0), java.util.Optional.of(resultado));
+    }
+
+    @Test
+    public void calculoFinalMaisDeUmRetorno() {
+        String entrada = "BRL100 CLP EUR";
+
+        Moeda moedaInicial = interpretadorDeEntrada.extrairMoedaInicial(entrada);
+        List<Moeda> moedasFinais = interpretadorDeEntrada.extrairMoedasFinais(entrada);
+
+        Double valor = interpretadorDeEntrada.extrairValor(entrada);
+        Double taxa = 0.0;
+        Double resultado = 0.0;
+        String saida = "";
+
+        for(Moeda moedaFinal : moedasFinais) {
+            taxa = interpretadorDeEntrada.detectarTaxaDeCambio(moedaInicial, moedaFinal);
+            resultado = calculadoraDeCambio.calcularValorFinal(valor, taxa);
+            saida = saida + formatadorDeSaida.formatarSaida(moedaFinal.getMoeda(), resultado) + " ";
+        }
+
+        Assert.assertEquals("CLP17565.0 EUR23.0 ", saida);
     }
 
 }
