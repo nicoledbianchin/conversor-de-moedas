aa1f5cfea03da614654194d382b6977846e38c36
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-03 13:43:35 -0300

    resolve metodo de retorno das moedas finais e refatora testes

--- src/main/java/tratamentoDeDados/InterpretadorDeEntrada.java
+++ src/main/java/tratamentoDeDados/InterpretadorDeEntrada.java
@@ -4,19 +4,20 @@ import valoresMoedasETaxas.Moeda;
 import valoresMoedasETaxas.TaxaDeCambio;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
 public class InterpretadorDeEntrada {
 
+    List<Moeda> moedasDisponiveis = new ArrayList<Moeda>(EnumSet.allOf(Moeda.class));
+    List<Moeda> moedasFinais = new ArrayList<>();
     Moeda moedaInicial;
-    List<Moeda> moedasIniciais = new ArrayList<Moeda>(EnumSet.allOf(Moeda.class));
     Double valor;
-    String moedaFinal;
 
     public Moeda extrairMoedaInicial(String entrada) {
         String moedaEntrada = entrada.substring(0, 3);
-        for (Moeda moeda : moedasIniciais){
+        for (Moeda moeda : moedasDisponiveis){
             if (moedaEntrada.equals(moeda.getMoeda())){
                 moedaInicial = moeda;
             }
@@ -29,46 +30,54 @@ public class InterpretadorDeEntrada {
         return valor;
     }
 
-    public String extrairMoedaFinal(String entrada){
-        String[] moeda = entrada.split(" ");
-        moedaFinal = moeda[1];
-        if (moeda.length > 2){
-            for (int i = 2; i < moeda.length; i++){
-                moedaFinal = moedaFinal + " " + moeda[i];
+    public List<Moeda> extrairMoedasFinais(String entrada){
+        List<String> moedasEntrada = Arrays.asList(entrada.split(" "));
+        for(String moeda : moedasEntrada){
+            for(Moeda moedaDisponivel : moedasDisponiveis) {
+                if(moeda.equals(moedaDisponivel.getMoeda())){
+                    moedasFinais.add(moedaDisponivel);
+                }
             }
-            return moedaFinal;
-        } else{
-            return moedaFinal;
         }
-    }
-
-
-    public double detectarTaxaDeCambio(Moeda moedaInicial, Moeda moedaFinal){
-        TaxaDeCambio taxaDeCambio = new TaxaDeCambio(moedaInicial);
-        double valor = taxaDeCambio.taxas.get(moedaFinal);
-        return valor;
-    }
-
-    public String detectarMoedaParaConversao(String origem, String destino){
-        if (destino.length() > 3){
-            return detectarMoedasParaConversao(origem, destino);
-        }
-        else {
-            return origem + destino;
-        }
-    }
-
-    public String detectarMoedasParaConversao(String origem, String destinos){
-        String[] lista = destinos.split(" ");
-        String moedas = origem + lista[0];
-        for (int i = 1; i < lista.length; i++){
-            moedas = moedas + " " + origem + lista[i];
-        }
-        return moedas;
+        return moedasFinais;
+//        moedaFinal = moeda[1];
+//        if (moeda.length > 2){
+//            for (int i = 2; i < moeda.length; i++){
+//                moedaFinal = moedaFinal + " " + moeda[i];
+//            }
+//            return moedaFinal;
+//        } else{
+//            return moedaFinal;
+//        }
     }
 
     public int retornaQuantidadeMoedasFinais(String moedasFinais) {
         String[] lista = moedasFinais.split(" ");
         return lista.length;
     }
+
+//    public double detectarTaxaDeCambio(Moeda moedaInicial, Moeda moedaFinal){
+//        TaxaDeCambio taxaDeCambio = new TaxaDeCambio(moedaInicial);
+//        double valor = taxaDeCambio.taxas.get(moedaFinal);
+//        return valor;
+//    }
+//
+//    public String detectarMoedaParaConversao(String origem, String destino){
+//        if (destino.length() > 3){
+//            return detectarMoedasParaConversao(origem, destino);
+//        }
+//        else {
+//            return origem + destino;
+//        }
+//    }
+//
+//    public String detectarMoedasParaConversao(String origem, String destinos){
+//        String[] lista = destinos.split(" ");
+//        String moedas = origem + lista[0];
+//        for (int i = 1; i < lista.length; i++){
+//            moedas = moedas + " " + origem + lista[i];
+//        }
+//        return moedas;
+
+//    }
 }
--- src/test/java/TestesInterpretadorDeEntrada.java
+++ src/test/java/TestesInterpretadorDeEntrada.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 import tratamentoDeDados.InterpretadorDeEntrada;
+import valoresMoedasETaxas.Moeda;
+
+import java.util.List;
 
 public class TestesInterpretadorDeEntrada {
 
@@ -10,9 +13,9 @@ public class TestesInterpretadorDeEntrada {
     public void retornaApenasMoedaInicial(){
         String entrada = "BRL110 CLP";
 
-        String moedaInicial = interpretador.extrairMoedaInicial(entrada);
+        Moeda moedaInicial = interpretador.extrairMoedaInicial(entrada);
 
-        Assert.assertEquals("BRL", moedaInicial);
+        Assert.assertEquals(Moeda.BRL, moedaInicial);
     }
 
     @Test
@@ -28,37 +31,38 @@ public class TestesInterpretadorDeEntrada {
     public void retornaUmaMoedaFinal(){
         String entrada = "BRL100 CLP";
 
-        String moedaFinal = interpretador.extrairMoedaFinal(entrada);
+        List<Moeda> moedaFinal = interpretador.extrairMoedasFinais(entrada);
 
-        Assert.assertEquals("CLP", moedaFinal);
+        Assert.assertEquals(Moeda.CLP, moedaFinal.get(0));
     }
 
     @Test
     public void retornaDuasMoedasFinais(){
         String entrada = "BRL100 CLP EUR";
 
-        String moedasFinais = interpretador.extrairMoedaFinal(entrada);
+        List<Moeda> moedasFinais = interpretador.extrairMoedasFinais(entrada);
 
-        Assert.assertEquals("CLP EUR", moedasFinais);
+        Assert.assertEquals(Moeda.CLP, moedasFinais.get(0));
+        Assert.assertEquals(Moeda.EUR, moedasFinais.get(1));
     }
 
-    @Test
-    public void retornaTodasMoedasFinais(){
-        String entrada = "BRL100 EUR USD CLP";
-
-        String moedasFinais = interpretador.extrairMoedaFinal(entrada);
-
-        Assert.assertEquals("EUR USD CLP", moedasFinais);
-    }
-
-    @Test
-    public void retornaQuantasMoedasFinaisSao(){
-        String moedasFinais = "BRL EUR USD";
-
-        int quantidadeMoedasFinais = interpretador.retornaQuantidadeMoedasFinais(moedasFinais);
-
-        Assert.assertEquals(3, quantidadeMoedasFinais);
-    }
+//    @Test
+//    public void retornaTodasMoedasFinais(){
+//        String entrada = "BRL100 EUR USD CLP";
+//
+//        String moedasFinais = interpretador.extrairMoedaFinal(entrada);
+//
+//        Assert.assertEquals("EUR USD CLP", moedasFinais);
+//    }
+//
+//    @Test
+//    public void retornaQuantasMoedasFinaisSao(){
+//        String moedasFinais = "BRL EUR USD";
+//
+//        int quantidadeMoedasFinais = interpretador.retornaQuantidadeMoedasFinais(moedasFinais);
+//
+//        Assert.assertEquals(3, quantidadeMoedasFinais);
+//    }
 
 //    @Test
 //    public void retornaMoedaInicialMaisFinal(){
