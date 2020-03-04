e9f6f3201247afead10912827d1c621d609c8352
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-04 12:59:55 -0300

    adiciona validador

---
+++ src/main/java/tratamentoDeDados/ValidadorMoeda.java
@@ -0,0 +1,23 @@
+package tratamentoDeDados;
+
+import valoresMoedasETaxas.*;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+public class ValidadorMoeda {
+    List<Moeda> moedasDisponiveis = new ArrayList<Moeda>(EnumSet.allOf(Moeda.class));
+    boolean valida = false;
+
+    public boolean validarMoeda(String moeda){
+        for (Moeda moedaValida : moedasDisponiveis) {
+            if (moedaValida.getMoeda().equals(moeda)) {
+                valida = true;
+//            } else if (!moedaValida.getMoeda().equals(moeda) && moedasDisponiveis.isEmpty()){
+//                valida = false;
+            }
+        }
+        return  valida;
+    }
+}
